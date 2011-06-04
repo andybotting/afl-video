702205e728b93294621410e7ff705422375c0fa6
Andy Botting <andy@andybotting.com>
2011-06-04 13:49:53 +0100

    Fixed AFL Radio plugin and included images

--- xbmcaddon/plugin.audio.afl-radio/default.py
+++ xbmcaddon/plugin.audio.afl-radio/default.py
@@ -4,7 +4,8 @@
 import xbmc, xbmcgui, xbmcplugin, os
 import urllib
 
-URL = "http://lon-cdn220-is-1.se.bptvlive.ngcdn.telstra.com/online-radio-afl_%s|"
+HOST = "fli-cdn220-is-1.se.bptvlive.ngcdn.telstra.com"
+URL = "http://" + HOST + "/online-radio-afl_%s|"
 USER_AGENT = urllib.urlencode({'User-Agent':'NSPlayer/11.08.0005.0000'})
 THUMB_PATH = os.path.join(os.getcwd(), "img", "radio_%s.jpg")
 
@@ -24,18 +25,63 @@ STATIONS = [
 		{ 'name':'K-Rock Geelong',                    'id':'15' },
 	]
 
-if ( __name__ == "__main__" ):
+__handle__ = int(sys.argv[1])
 
+def get_params(s):
+	dict = {}
+	pairs = s.lstrip("?").split("&")
+	for pair in pairs:
+		if len(pair) < 3: continue
+		kv = pair.split("=",1)
+		k = kv[0]
+		v = urllib.unquote_plus(kv[1])
+		dict[k] = v
+	return dict
+
+def make_params(d):
+	pairs = []
+	for k,v in d.iteritems():
+		k = urllib.quote_plus(k)
+		v = urllib.quote_plus(str(v))
+		pairs.append("%s=%s" % (k,v))
+	return "&".join(pairs)
+
+def list_stations():
 	for station in STATIONS:
-		name = station['name']
-		url = (URL % station['id']) + USER_AGENT
 		thumb = THUMB_PATH % station['id']
+		listitem = xbmcgui.ListItem(station['name'])
+		labels = { "title": station['name'], "genre": "Sport" }
+		listitem.setInfo('audio', infoLabels=labels)
+		listitem.setThumbnailImage(thumb)
+		params = make_params({"id": station['id'], "name": station['name']})
+		url = "%s?%s" % (sys.argv[0], params)
+		xbmcplugin.addDirectoryItem(handle=__handle__, url=url, listitem=listitem)
 
-		listitem = xbmcgui.ListItem(name)
-		listitem.setInfo('audio', {"genre": "sport"})
-		#listitem.setThumbnailImage(thumb)
-		# Add the program item to the list
-		xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url, listitem=listitem)
+	xbmcplugin.endOfDirectory(handle=__handle__, succeeded=True)
 
-	xbmcplugin.endOfDirectory(handle=int(sys.argv[1]), succeeded=True)
+def play(params):
+	p = get_params(params)
+
+	pDialog = xbmcgui.DialogProgress()
+	pDialog.create('AFL Radio', 'Starting ' +  p['name'], 'Please wait, this might take a while...')
+	pDialog.update(50)
+
+	thumb = THUMB_PATH % p['id']
+	labels = { "title": p['name'], "genre": "Sport" }
+	listitem = xbmcgui.ListItem(p['name'])
+	listitem.setInfo('audio', infoLabels=labels)
+	listitem.setThumbnailImage(thumb)
+	url = 'http://fli-cdn220-is-1.se.bptvlive.ngcdn.telstra.com/online-radio-afl_' + p['id'] + '?WMBitrate=262144|' + urllib.urlencode({'User-Agent':'NSPlayer/11.08.0005.0000'})
+	xbmc.Player(xbmc.PLAYER_CORE_AUTO).play(url, listitem)
+	pDialog.close()
+
+
+if __name__ == "__main__" :
+	params = sys.argv[2]
+	p = get_params(params)
+
+	if p.has_key("id"):
+		play(params)
+	else:
+		list_stations()
 
